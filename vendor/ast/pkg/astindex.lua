3136f0698f191dca778e29770fa95f4ccd2b14e3
Oleg Kolosov <bazurbat@gmail.com>
2018-04-02 16:29:36 +0200

    Adjust rules to sync with core changes

--- vendor/ast/pkg/astindex.lua
+++ vendor/ast/pkg/astindex.lua
@@ -2,6 +2,7 @@ return {
     source = {
         type     = 'hg',
         location = 'ssh://hg@bitbucket.org/art-system/astindex',
-        dir      = 'karaoke-player/source/astindex'
-    }
+        dir      = '$karaoke_player_source_dir/source/astindex'
+    },
+    use = { 'karaoke-player' }
 }
